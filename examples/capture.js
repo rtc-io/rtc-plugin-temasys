7dfa4cdd7cb95745284791d131c9dc9babac29c0
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-14 11:29:52 +1000

    Initial commit

---
+++ examples/capture.js
@@ -0,0 +1,13 @@
+// require the media capture helper from rtc.io
+var media = require('rtc-media');
+
+// capture the local media, letting rtc-media know it can use
+// the temasys plugin
+var localMedia = media({
+  plugins: [
+    require('../')
+  ]
+});
+
+// render the media to the document body
+localMedia.render(document.body);
---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+  # rtc-plugin-temasys
+
+  This is an experimental plugin for bridging the functionality provided by
+  the [Temasys IE plugin](http://bit.ly/1lnlEIK) into the rtc.io module
+  suite.
+
+  ## Example Usage
+
+  A simple capture example is shown below:
+
+  <<< examples/capture.js
+
+  ## Reference
+
+  Documented below is an number of plugin functions that are used to
+  both determine plugin suitability for a browser / platform and also
+  ensure that the plugin is correctly loaded into the HTML document.
+
+**/
+
+exports.supported = function(platform) {
+};
