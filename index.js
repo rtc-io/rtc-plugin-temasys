c111cad360d24a8bf2005d163d171fb4003477a0
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-14 15:35:50 +1000

    Plugin invocation working

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/* jshint node: true */
+'use strict';
+
+var crel = require('crel');
+
 /**
   # rtc-plugin-temasys
 
@@ -19,5 +24,80 @@
 
 **/
 
+var PLUGIN_MIMETYPE = 'application/x-temwebrtcplugin';
+var counter = 0;
+
+/**
+  ### supported(platform) => Boolean
+
+  The supported function returns true if the platform (as detected using
+  `rtc-core/detect`) is compatible with the plugin. By doing this prelimenary
+  detection you can specify a number of plugins to be loaded but only
+  the first the is supported on the current platform will be used.
+
+**/
 exports.supported = function(platform) {
+  return ['ie', 'safari', 'chrome'].indexOf(platform.browser.toLowerCase()) >= 0;
+};
+
+/**
+  ### init(callback)
+
+  The `init` function is reponsible for ensuring that the current HTML
+  document is prepared correctly.
+
+**/
+var init = exports.init = function(callback) {
+  // find the temasys plugin
+  var plugin = document.querySelector('object[type="' + PLUGIN_MIMETYPE + '"]');
+  var pluginId = '__temasys_plugin' + (counter++);
+  var params = [
+    { name: 'onload', value: '__load' + pluginId },
+    { name: 'pluginId', value: pluginId }
+  ];
+
+  // patch in the onload handler into the window object
+  window['__load' + pluginId] = function() {
+    console.log('plugin loaded');
+  };
+
+  console.log('initializing plugin: ' + pluginId);
+
+  // if the plugin is not found, then add it to the document
+  if (! plugin) {
+    plugin = crel('object', {
+      width: 0,
+      height: 0,
+      type: PLUGIN_MIMETYPE,
+      id: pluginId
+    });
+
+    // create the plugin parameters
+    params.forEach(function(data) {
+      plugin.appendChild(crel('param', data));
+    });
+
+    // add the plugin to the document body
+    document.body.appendChild(plugin);
+  }
+};
+
+/**
+  ### initMedia(media, callback)
+
+  The `initMedia` function is to perform two functions:
+
+  1. To ensure that the HTML document has been prepared correctly for
+     the plugin.  If not, the `init` function will be called.
+
+  2. To apply any plugin specific logic into the rtc.io media capture
+     interface.
+
+**/
+exports.initMedia = function(media, callback) {
+  init(function(err) {
+    if (err) {
+      return callback(err);
+    }
+  })
 };
